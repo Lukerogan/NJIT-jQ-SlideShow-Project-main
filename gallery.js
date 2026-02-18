dd05d758305373e5abafb8aeb7fd790594a64b6c
Luke <126lrogan@frhsd.com>
2026-02-18 08:37:20 -0500

    Implement AJAX request to load JSON data

--- gallery.js
+++ gallery.js
@@ -1,5 +1,5 @@
 let mCurrentIndex = 0 // Tracks the current image index
-let mImages = [] // Array to hold GalleryImage objects
+let mImages = [$.getJSON()] // Array to hold GalleryImage objects
 const mUrl = 'https://your-json-url.com' // Replace with actual JSON URL
 const mWaitTime = 5000 // Timer interval in milliseconds
 
