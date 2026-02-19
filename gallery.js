1bbbb914960a68deebd723b9673dca5d5678fae2
Luke <126lrogan@frhsd.com>
2026-02-19 09:48:28 -0500

    step-4-i-not-done

--- gallery.js
+++ gallery.js
@@ -1,6 +1,6 @@
 let mCurrentIndex = 0 // Tracks the current image index
 let mImages = [] // Array to hold GalleryImage objects
-const mUrl = 'myImages.json' // Replace with actual JSON URL
+const mUrl = 'myImages.json'; // Replace with actual JSON URL
 const mWaitTime = 5000 // Timer interval in milliseconds
 
 $(document).ready(() => {
@@ -22,15 +22,15 @@ $(document).ready(() => {
 
 // Function to fetch JSON data and store it in mImages
 function fetchJSON () {
- $.getJSON()
- let mImages = [JSON.parse(mUrl)]
-  // After JSON is loaded, call swapPhoto() to display the first image
+ $.getJSON(mUrl, function(data) {mImages = data})
+console.log(mImages)
+  swapPhoto()  // After JSON is loaded, call swapPhoto() to display the first image
 }
 
 // Function to swap and display the next photo in the slideshow
 function swapPhoto () {
   // Access mImages[mCurrentIndex] to update the image source and details
-  // Update the #photo element's src attribute with the current image's path
+  $('#photo').attr('src', '');// Update the #photo element's src attribute with the current image's path
   // Update the .location, .description, and .date elements with the current image's details
 }
 
