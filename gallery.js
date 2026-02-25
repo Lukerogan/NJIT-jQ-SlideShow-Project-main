51222a54a1fa1462f5ee3b80343b198fe37b9db3
Lukerogan <126Lrogan@frhsd.com>
2026-02-25 15:25:30 -0500

    Implement next and previous button functionality

--- gallery.js
+++ gallery.js
@@ -6,17 +6,20 @@ const mWaitTime = 5000 // Timer interval in milliseconds
 $(document).ready(() => {
   $('.details').hide() // Hide details initially
 
-  // Call a function here to start the timer for the slideshow
+  startTimer()// Call a function here to start the timer for the slideshow
 
   // Select the moreIndicator button and add a click event to:
   // - toggle the rotation classes (rot90 and rot270)
-  // - slideToggle the visibility of the .details section
+   $('.moreIndicator').on('click', function() {
+    $(this).toggleClass('rot90 rot270')
+    $('.details').slideToggle()
+  })// - slideToggle the visibility of the .details section
 
-  // Select the "Next Photo" button and add a click event to call showNextPhoto
+   $('#nextPhoto').on('click', showNextPhoto)// Select the "Next Photo" button and add a click event to call showNextPhoto
 
-  // Select the "Previous Photo" button and add a click event to call showPrevPhoto
+    $('#prevPhoto').on('click', showPrevPhoto)// Select the "Previous Photo" button and add a click event to call showPrevPhoto
 
-  // Call fetchJSON() to load the initial set of images
+    fetchJSON()// Call fetchJSON() to load the initial set of images
   fetchJSON()
 })
 
@@ -42,13 +45,15 @@ function swapPhoto() {
 // Advances to the next photo, loops to the first photo if the end of array is reached
 function showNextPhoto() {
   // Increment mCurrentIndex and call swapPhoto()
-  // Ensure it loops back to the beginning if mCurrentIndex exceeds array length
+  mCurrentIndex = (mCurrentIndex + 1) % mImages.length;
+  swapPhoto();// Ensure it loops back to the beginning if mCurrentIndex exceeds array length
 }
 
 // Goes to the previous photo, loops to the last photo if mCurrentIndex goes negative
 function showPrevPhoto() {
   // Decrement mCurrentIndex and call swapPhoto()
-  // Ensure it loops to the end if mCurrentIndex is less than 0
+   mCurrentIndex = (mCurrentIndex - 1 + mImages.length) % mImages.length;
+  swapPhoto();// Ensure it loops to the end if mCurrentIndex is less than 0
 }
 
 // Starter code for the timer function
